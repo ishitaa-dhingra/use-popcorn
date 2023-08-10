28fb270784b0c85b6f7a8ae3aaaa7dee5139bbf4
Ishitaa Dhingra <ishitaadhingra@gmail.com>
2023-08-10 19:37:32 +0530

    making star rating reusable

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovierating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxrating={10} onsetRating={setMovierating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
- 
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={23} color="purple" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
